c1b43d85fc1f15de6d63fd62e5b52e921bc9365d
mariumyounus <mariumyounus4@gmail.com>
2024-04-22 01:02:19 +0100

    deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+'/<Real-Estate.git>/'
 export default defineConfig({
   plugins: [react()],
 })
